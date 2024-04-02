50508770de7167844100f48de67db0ee3a5a0eb2
Kevin Chen <86860267+kchprog@users.noreply.github.com>
2024-04-02 16:06:12 -0400

    Update script.py

--- script.py
+++ script.py
@@ -88,8 +88,8 @@ def plot_fourier_transform(audioArray, sampleRate):
         fft_result = 20 * np.log10(np.abs(fft_result))
 
     ## MIN distance between peaks (to stop us from picking clustered peaks)
-    min_distance = 600
-    min_prominence = 0.3
+    min_distance = 100
+    min_prominence = 0.2
 
     ## Find peaks
     peaks, _ = find_peaks(fft_result, prominence=min_prominence, distance=min_distance)
